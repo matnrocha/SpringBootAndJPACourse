90ba140915a15d9b2165a69ca77e921d111f8d47
matro0 <mateusanroc@gmail.com>
2024-07-01 03:00:47 +0000

    feat: create Path variable based mapping and request parameters

--- springproject/src/main/java/com/mateus/springproject/FirstController.java
+++ springproject/src/main/java/com/mateus/springproject/FirstController.java
@@ -20,9 +20,26 @@ public class FirstController {
     @PostMapping("/post-order")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public String post(@RequestBody Order order){
-        String response = "Request accepted in post-order: " + order.getProductName() + order.getCustomerName() + order.getQuantity();
-        return response;
+        return "Request accepted in post-order: " + order.getProductName() + order.getCustomerName() + order.getQuantity();
     }
+
+
+    // http://localhost:8080/hello/{username}
+    @GetMapping("/hello/{username}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String pathVar(@PathVariable("username") String username){
+        return "Request accepted in post-order: " + username;
+    }
+
+    // http://localhost:8080/hello?param1=value1&param2=value2
+    @GetMapping("/hello")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String RequestParam(@RequestParam("firstName") String firstName,
+                          @RequestParam("lastName") String lastName){
+        return "Request accepted in post-order: " + firstName + " " + lastName;
+    }
+
+
 
 
 }
