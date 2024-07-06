f75552950d4c5a5b3fd14e139e5b28268d2d6f5a
matro0 <mateusanroc@gmail.com>
2024-07-06 18:19:06 +0000

    feat: insert validation on studentDto and exception handler with custom messages

--- springproject/src/main/java/com/mateus/springproject/student/StudentDto.java
+++ springproject/src/main/java/com/mateus/springproject/student/StudentDto.java
@@ -1,7 +1,11 @@
 package com.mateus.springproject.student;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record StudentDto(
+        @NotEmpty(message = "first name should not be empty")
         String firstName,
+        @NotEmpty(message = "Last name should not be empty")
         String lastName,
         String email,
         Integer schoolId
