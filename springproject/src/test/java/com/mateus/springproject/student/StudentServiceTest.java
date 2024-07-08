0e2b331712465d807e407fde9219307db5d3acf4
matro0 <mateusanroc@gmail.com>
2024-07-08 02:23:04 +0000

    Test: Add unit tests should_return_all_students and should_find_list_of_students_by_first_name and should_delete_student_by_id

--- springproject/src/test/java/com/mateus/springproject/student/StudentServiceTest.java
+++ springproject/src/test/java/com/mateus/springproject/student/StudentServiceTest.java
@@ -7,6 +7,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -77,5 +82,119 @@ class StudentServiceTest {
                 .save(student);
         verify(studentMapper, times(1))
                 .toStudentResponseDto(savedStudent);
+    }
+
+    @Test
+    public void should_return_all_students(){
+        List<Student> students = new ArrayList<>();
+        School school = new School("ufpe");
+        students.add(new Student(
+                "mateus",
+                "rocha",
+                "mateus@gmail.com",
+                21,
+                school
+        ));
+
+        when(studentRepository.findAll()).thenReturn(students);
+        when(studentMapper.toStudentResponseDto(any(Student.class))).thenReturn(new StudentResponseDto(
+                "mateus",
+                "rocha",
+                "mateus@gmail.com"
+        ));
+
+        List<StudentResponseDto> dtos = studentService.findAllStudents();
+
+        assertEquals(students.size(), dtos.size());
+        verify(studentRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void should_return_student_by_id(){
+        Integer studentId = 1;
+        School school = new School("ufpe");
+        Student student = new Student(
+                "mateus",
+                "rocha",
+                "mateus@gmail.com",
+                21,
+                school
+        );
+
+        when(studentRepository.findById(studentId))
+                .thenReturn(Optional.of(student));
+        when(studentMapper.toStudentResponseDto(any(Student.class)))
+                .thenReturn(new StudentResponseDto(
+                        "mateus",
+                        "rocha",
+                        "mateus@gmail.com"
+                ));
+
+        StudentResponseDto dto = studentService.findStudentById(studentId);
+
+        assertEquals(dto.firstName(), student.getFirstName());
+        assertEquals(dto.lastName(), student.getLastName());
+        assertEquals(dto.email(), student.getEmail());
+
+        verify(studentRepository, times(1)).findById(studentId);
+    }
+
+    @Test
+    public void should_find_list_of_students_by_first_name(){
+        String studentName = "mateus";
+        List<Student> students = new ArrayList<>();
+
+        School school = new School("ufpe");
+        Student student1 = new Student(
+                "mateus",
+                "rocha",
+                "mateus@gmail.com",
+                21,
+                school
+        );
+        Student student2 = new Student(
+                "mateuso",
+                "rocha",
+                "mateusrocha@gmail.com",
+                23,
+                school
+        );
+
+        students.add(student1);
+        students.add(student2);
+
+        when(studentRepository.findAllByFirstNameContaining(studentName))
+                .thenReturn(students);
+
+        when(studentMapper.toStudentResponseDto(any(Student.class)))
+                .thenReturn(new StudentResponseDto(
+                        "mateus",
+                        "rocha",
+                        "mateus@gmail.com"
+                ));
+
+        List<StudentResponseDto> dtos = studentService.findAllStudentsByFirstName(studentName);
+
+        assertEquals(dtos.size(), students.size());
+        verify(studentRepository, times(1)).findAllByFirstNameContaining(studentName);
+    }
+
+    @Test
+    public void should_delete_student_by_id(){
+        Integer id = 1;
+        Student student = new Student(
+                "mateuso",
+                "rocha",
+                "mateusrocha@gmail.com",
+                23,
+                new School("ufpe")
+        );
+
+        when(studentRepository.deleteById(id))
+                .thenReturn();
+
+        studentService.delete(id);
+        
+        verify(studentRepository, times(1)).findById(id);
     }
 }
